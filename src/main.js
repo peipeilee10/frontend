fa09016c3aced6280f8a866d4dee22e4a1743eb0
peipeilee0910 <vicky607vicky607@gmail.com>
2022-01-17 08:47:37 +0800

    favicon

--- src/main.js
+++ src/main.js
@@ -11,6 +11,11 @@ import 'sweetalert2/dist/sweetalert2.min.css'
 import mixin from './mixin.js'
 import ImgInputer from 'vue-img-inputer'
 import 'vue-img-inputer/dist/index.css'
+import VueGtag from 'vue-gtag'
+
+Vue.use(VueGtag, {
+  config: { id: 'G-BJFLJWGFWE' }
+})
 
 Vue.use(VueSweetalert2)
 Vue.mixin(mixin)
